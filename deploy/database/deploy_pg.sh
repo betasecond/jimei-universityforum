6697cc51737373e0b061a0faf1bb9f61694d3691
betasecond <nbdhc9039@gmail.coom>
2024-03-30 10:08:08 +0800

    docker for pgsql build refactor

--- deploy/database/deploy_pg.sh
+++ deploy/database/deploy_pg.sh
@@ -5,19 +5,31 @@ default_project_name="jimei-universityforum"
 if [ "$#" -ne 1 ]; then
     echo "No project name provided. Using default: ${default_project_name}"
     project_name="${default_project_name}"
-    exit 1
+
 else
   # 设置项目名称变量
   project_name="$1"
 fi
 
+# 启用 Docker BuildKit
+export DOCKER_BUILDKIT=1
 
+# 过去
+#sudo docker build -t my-custom-postgres:13 .
 
-sudo docker build -t my-custom-postgres:13 .
+# 使用 BuildKit
+sudo -E docker build -t my-custom-postgres:13
+
+# 创建挂载目录
 sudo mkdir -p "/opt/${project_name}/postgres/data"
 sudo mkdir -p "/opt/${project_name}/postgres/init-scripts"
+
+#
 sudo docker run --name "${project_name}-postgres" -d \
     -p 6000:5432 \
     -v "/opt/${project_name}/postgres/data:/var/lib/postgresql/data" \
     -v "/opt/${project_name}/postgres/init-scripts:/docker-entrypoint-initdb.d/"  \
     my-custom-postgres:13
+
+#
+sudo docker ps
