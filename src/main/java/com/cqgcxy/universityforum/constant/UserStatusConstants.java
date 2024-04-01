dae07a4f0767477ac8d68e283b0a50758a59b423
betasecond <nbdhc9039@gmail.coom>
2024-04-01 12:48:28 +0800

    remove git

--- src/main/java/com/cqgcxy/universityforum/constant/UserStatusConstants.java
+++
@@ -1,6 +0,0 @@
-package com.cqgcxy.universityforum.constant;
-
-public class UserStatusConstants {
-    public static final byte ACTIVE = 1;
-    public static final byte INACTIVE = 0;
-}
--- src/main/java/com/cqgcxy/universityforum/service/AdminService.java
+++
@@ -1,5 +0,0 @@
-package com.cqgcxy.universityforum.service;
-
-public interface AdminService {
-    boolean adminLogin(String username,String password);
-}
